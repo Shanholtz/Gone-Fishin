6fc099bdcd8d380bdf55223d4b156c97986467f5
Ashton Shanholtz <adshanholtz@outlook.com>
2025-04-25 12:36:07 -0400

    Cleaned up files and fixed AI card matching
    Deleted old temporary assets and fixed bug where AI wouldnt registar any pairs in their hand unless pair is recieved by player.

--- Assets/Scripts/CardGame/TurnManager.cs
+++ Assets/Scripts/CardGame/TurnManager.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TurnManager : MonoBehaviour
 {
 
     public PlayerManager playerHand;
     public AIManager aiHand;
+    public GameObject request;
 
     public bool isPlayerTurn = false; // Player starts first
 
@@ -23,12 +25,14 @@ public class TurnManager : MonoBehaviour
             Debug.Log("Player's Turn");
             playerHand.isTurn = true;
             aiHand.isTurn = false;
+            request.SetActive(true);
         }
         else
         {
             Debug.Log("AI's Turn");
             playerHand.isTurn = false;
             aiHand.isTurn = true;
+            request.SetActive(false);
             aiHand.AIRequestMatch();
         }
     }
